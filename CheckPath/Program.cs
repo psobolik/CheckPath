504210ba0720b996fac7a98715ed2d089c5d9b74
Paul Sobolik <psobolik@zoho.com>
2022-04-12 17:12:42 -0400

    Initial commit

---
+++ CheckPath/Program.cs
@@ -0,0 +1,44 @@
+﻿var path = GetPath().ToArray();
+WriteValues("PATH:", path);
+WriteValues("Missing folders in PATH:", path.Where(folder => !Directory.Exists(PathWithExpandedTilde(folder))).ToArray());
+WriteValues("Duplicate folders in PATH:", path.Where((item) => path.Count(i => i == item) > 1).Distinct().ToArray());
+
+static string PathWithExpandedTilde(string str)
+{
+    return str.StartsWith('~')
+        ? string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), str.AsSpan(1))
+        : str;
+}
+
+static IEnumerable<string> GetPath()
+{
+    var path = Environment.GetEnvironmentVariable("PATH");
+    return string.IsNullOrWhiteSpace(path) ? Array.Empty<string>() : path.Split(Path.PathSeparator);
+}
+
+static void WriteValues(string header, string[] values)
+{
+    const ConsoleColor headerColor = ConsoleColor.Green;
+    const ConsoleColor infoColor = ConsoleColor.Yellow;
+    const ConsoleColor listColor = ConsoleColor.White;
+        
+    static void WriteValue(string value, ConsoleColor foregroundColor)
+    {
+        Console.ForegroundColor = foregroundColor;
+        Console.WriteLine(value);
+        Console.ResetColor();
+    }
+
+    WriteValue(header, headerColor);
+    if (values.Any())
+    {
+        foreach (var value in values)
+        {
+            WriteValue(value, listColor);
+        }
+    }
+    else
+    {
+        WriteValue("[None]", infoColor);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CheckPath
+
+A simple .NET Core console application that examines your PATH environment 
+variable and prints any folders in it that don't exist or that appear more 
+than once.
+
+A simpler, more Linq-y version of ckpath.
+
+License: [MIT](LICENSE.md)
